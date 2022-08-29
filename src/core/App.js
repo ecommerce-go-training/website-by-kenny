7b68411cb50ec0b4e7be6cb55c0a54f83f269207
kenny-phan-goldenowl <kenny.phan.goldenowl@gmail.com>
2022-08-29 11:29:41 +0700

    add packages: react-select/form/ detect-offline/ etc

--- src/core/App.js
+++ src/core/App.js
@@ -1,9 +1,13 @@
+import { Online, Offline } from 'react-detect-offline';
+
 function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <Online>Show when online</Online>
+            <Offline>Show when offline</Offline>
+        </div>
+    );
 }
 
 export default App;
