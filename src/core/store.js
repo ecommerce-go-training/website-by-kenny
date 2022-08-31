680bed9ec889da115368b76795a31598fc78776c
kenny-phan-goldenowl <kenny.phan.goldenowl@gmail.com>
2022-08-31 17:46:33 +0700

    add redux auth

--- src/core/store.js
+++ src/core/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import counterReducer from 'global/redux/counter/slice';
+import authReducer from 'global/redux/auth/slice';
 
 const store = configureStore(
   {
     reducer: {
       counter: counterReducer,
+      auth   : authReducer,
     },
   },
   composeWithDevTools()
