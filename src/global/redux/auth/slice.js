75df9ecd126de968752c48a82d3225742f90021c
kenny-phan-goldenowl <kenny.phan.goldenowl@gmail.com>
2022-08-31 17:59:47 +0700

    add redux auth

--- src/global/redux/auth/slice.js
+++ src/global/redux/auth/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { registerUser } from './authActions';
 
 const initialState = {
   loading  : false,
@@ -9,9 +10,23 @@ const initialState = {
 };
 
 const authSlice = createSlice({
-  name    : 'auth',
+  name         : 'auth',
   initialState,
-  reducers: {},
+  reducers     : {},
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [registerUser.fulfilled]: (state) => {
+      state.loading = false;
+      state.success = true;
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+  },
 });
 
 export default authSlice.reducer;
