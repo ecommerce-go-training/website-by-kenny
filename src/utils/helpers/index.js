6bd6afcbf703ab3e4404b41a186d656610673caf
kenny-phan-goldenowl <kenny.phan.goldenowl@gmail.com>
2022-10-28 12:19:07 +0700

    add vi language

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -1,12 +1,8 @@
 import { toast } from 'react-toastify';
 
-const saveLoginToken = (token) => {
-  localStorage.setItem('token', token);
-};
+const saveLoginToken = (token) => localStorage.setItem('token', token);
 
-const removeLoginToken = () => {
-  localStorage.removeItem('token');
-};
+const removeLoginToken = () => localStorage.removeItem('token');
 
 const formatCurrency = (number) => {
   const currency = new Intl.NumberFormat(undefined, {
@@ -19,10 +15,13 @@ const formatCurrency = (number) => {
 const showErrorNoti = (message) => toast.error(message);
 const showSuccessNoti = (message) => toast.success(message);
 
+const showNoti = (type, message) => toast?.[type](message);
+
 export {
   formatCurrency,
   removeLoginToken,
   saveLoginToken,
   showErrorNoti,
+  showNoti,
   showSuccessNoti,
 };
