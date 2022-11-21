fb5a52a7d0b89cb88e5ba69ea3bc6f3e7650e0c2
kenny-phan-goldenowl <kenny.phan.goldenowl@gmail.com>
2022-11-21 17:41:38 +0700

    add loading while wating for product details

--- src/pages/ItemDetails/index.js
+++ src/pages/ItemDetails/index.js
@@ -8,6 +8,7 @@ import Button from 'components/Button';
 import Collapse from 'components/Collapse';
 import Slider2 from 'components/Sliderv2';
 import Slider3, { Slider3Item } from 'components/Sliderv3';
+import Loading from 'components/Loading';
 import WaitlistForm from './WaitlistForm';
 
 import { formatCurrency } from 'utils/helpers';
@@ -34,7 +35,9 @@ const ItemDetails = () => {
 
   const dispatch = useDispatch();
 
-  const { currentProduct, productList } = useSelector((state) => state.product);
+  const { currentProduct, productList, isLoading } = useSelector(
+    (state) => state.product
+  );
   const { cartItem } = useSelector((state) => state.cart);
   const { id } = useParams();
 
@@ -107,119 +110,123 @@ const ItemDetails = () => {
             </div>
           ))}
         </div>
-        <div className='details__info'>
-          <p>{currentProduct?.category?.name}</p>
-          <p>{currentProduct?.name}</p>
-          <p>
-            {formatCurrency('VND', currentProduct.totalPrice)}
-            {currentProduct?.discount?.status && (
-              <span className='old-price'>
-                {' '}
-                {formatCurrency('VND', currentProduct?.price)}
-              </span>
-            )}
-          </p>
-          <p>{currentProduct?.description}</p>
-          <div className='details__info-filter'>
-            <div>
-              <p>size</p>
+        {isLoading ? (
+          <Loading alter />
+        ) : (
+          <div className='details__info'>
+            <p>{currentProduct?.category?.name}</p>
+            <p>{currentProduct?.name}</p>
+            <p>
+              {formatCurrency('VND', currentProduct.totalPrice)}
+              {currentProduct?.discount?.status && (
+                <span className='old-price'>
+                  {' '}
+                  {formatCurrency('VND', currentProduct?.price)}
+                </span>
+              )}
+            </p>
+            <p>{currentProduct?.description}</p>
+            <div className='details__info-filter'>
               <div>
-                {currentProduct?.sizeColorList?.size.map((item, index) => (
-                  <p
-                    key={index}
-                    className={size === item ? 'active' : ''}
-                    onClick={() => {
-                      setSize(item);
-                    }}
-                  >
-                    {item}
-                  </p>
-                ))}
+                <p>size</p>
+                <div>
+                  {currentProduct?.sizeColorList?.size.map((item, index) => (
+                    <p
+                      key={index}
+                      className={size === item ? 'active' : ''}
+                      onClick={() => {
+                        setSize(item);
+                      }}
+                    >
+                      {item}
+                    </p>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <p>color</p>
+                <div className='product-color-div'>
+                  {currentProduct?.sizeColorList?.color.map((item, index) => (
+                    <div
+                      key={index}
+                      className='product-color'
+                      style={{ backgroundColor: item }}
+                      onClick={() => setColor(item)}
+                    >
+                      {color === item && (
+                        <img
+                          className='blackCheck-icon'
+                          src={blackCheck}
+                          alt='icon image'
+                        />
+                      )}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-            <div>
-              <p>color</p>
-              <div className='product-color-div'>
-                {currentProduct?.sizeColorList?.color.map((item, index) => (
-                  <div
-                    key={index}
-                    className='product-color'
-                    style={{ backgroundColor: item }}
-                    onClick={() => setColor(item)}
-                  >
-                    {color === item && (
-                      <img
-                        className='blackCheck-icon'
-                        src={blackCheck}
-                        alt='icon image'
-                      />
+            <Button
+              handleClick={
+                currentProduct?.inventories
+                  ? () =>
+                    handleAddItem({
+                      ...currentProduct,
+                      size    : size,
+                      color   : color,
+                      quantity: 1,
+                      // image   : img.mainImage
+                      image   : img,
+                    })
+                  : handleWaitForm
+              }
+            >
+              <p>
+                {currentProduct?.inventories ? t('addToCart') : t('waitList')}
+              </p>
+            </Button>
+            {toggleWaitForm && (
+              <WaitlistForm
+                imageName={currentProduct?.name}
+                closeForm={() => setToggleWaitForm(false)}
+              />
+            )}
+            <div className='details__info-faq'>
+              <div>
+                <Collapse smallLabel label='product details'>
+                  <p className='info-item'>{currentProduct?.detail}</p>
+                </Collapse>
+              </div>
+              <div>
+                <Collapse smallLabel label='size & fit'>
+                  <div className='info-item'>
+                    {t('findYourSize')}{' '}
+                    <span onClick={() => setToggleSizeGuide(true)}>Here</span>
+                    {toggleSizeGuide && (
+                      <QuickSizeGuide toggle={setToggleSizeGuide} />
                     )}
                   </div>
-                ))}
+                </Collapse>
+              </div>
+              <div>
+                <Collapse smallLabel label='shipping & returns'>
+                  <p className='info-item'>
+                    {t('shipping')}{' '}
+                    <Link to='/customer-support/shipping'>
+											shipping & returns
+                    </Link>
+                  </p>
+                </Collapse>
+              </div>
+              <div>
+                <Collapse smallLabel label='garment care'>
+                  {currentProduct?.garmentCare
+                    ? currentProduct?.garmentCare
+                    : 'Wash with hand'}
+                </Collapse>
               </div>
             </div>
           </div>
-          <Button
-            handleClick={
-              currentProduct?.inventories
-                ? () =>
-                  handleAddItem({
-                    ...currentProduct,
-                    size    : size,
-                    color   : color,
-                    quantity: 1,
-                    // image   : img.mainImage
-                    image   : img,
-                  })
-                : handleWaitForm
-            }
-          >
-            <p>
-              {currentProduct?.inventories ? t('addToCart') : t('waitList')}
-            </p>
-          </Button>
-          {toggleWaitForm && (
-            <WaitlistForm
-              imageName={currentProduct?.name}
-              closeForm={() => setToggleWaitForm(false)}
-            />
-          )}
-          <div className='details__info-faq'>
-            <div>
-              <Collapse smallLabel label='product details'>
-                <p className='info-item'>{currentProduct?.detail}</p>
-              </Collapse>
-            </div>
-            <div>
-              <Collapse smallLabel label='size & fit'>
-                <div className='info-item'>
-                  {t('findYourSize')}{' '}
-                  <span onClick={() => setToggleSizeGuide(true)}>Here</span>
-                  {toggleSizeGuide && (
-                    <QuickSizeGuide toggle={setToggleSizeGuide} />
-                  )}
-                </div>
-              </Collapse>
-            </div>
-            <div>
-              <Collapse smallLabel label='shipping & returns'>
-                <p className='info-item'>
-                  {t('shipping')}{' '}
-                  <Link to='/customer-support/shipping'>
-										shipping & returns
-                  </Link>
-                </p>
-              </Collapse>
-            </div>
-            <div>
-              <Collapse smallLabel label='garment care'>
-                {currentProduct?.garmentCare
-                  ? currentProduct?.garmentCare
-                  : 'Wash with hand'}
-              </Collapse>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
       <div className='moreItem'>
         <p>{t('more')}</p>
