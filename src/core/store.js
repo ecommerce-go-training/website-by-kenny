96be2f4d7e3bd5066d95e0d1d4acff0bf5ebef92
kenny-phan-goldenowl <kenny.phan.goldenowl@gmail.com>
2022-11-23 14:17:23 +0700

    update feedbacks

--- src/core/store.js
+++ src/core/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import Env from 'config/env';
 
 import rootReducer from './reducers';
 
 const store = configureStore(
   {
     reducer   : rootReducer,
+    devTools  : Env.NODE_ENV !== 'production',
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
         serializableCheck: false,
